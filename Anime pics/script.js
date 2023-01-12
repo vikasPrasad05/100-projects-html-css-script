57dc581f3f846567a1fb39a562556a961a0a4f26
vikasprasad05 <vikasprasad888999@gmail.com>
2023-01-12 21:51:51 +0530

    anime gallery

--- Anime pics/script.js
+++ Anime pics/script.js
@@ -1,2 +1,22 @@
 const btn = document.getElementById("btn")
-const 
+const anime = document.querySelector(".anime");
+const animeimg = document.querySelector(".animeimg");
+const nameanime = document.querySelector(".name");
+
+btn.addEventListener("click",async function(){
+    try {
+        btn.ariaDisabled = true;
+        btn.innerText = "loading..."
+        nameanime.innerText = "updating..."
+        animeimg.src = "spinner.svg";
+        const response = await fetch("https://api.catboys.com/img")
+        const data = await response.json()
+        btn.ariaDisabled = false;
+        btn.innerText = "Get pics";
+        anime.style.display = "block"
+        animeimg.src = data.url;
+        nameanime.innerText = data.artist;
+    } catch (error) {
+        console.log(error)
+    }
+})
