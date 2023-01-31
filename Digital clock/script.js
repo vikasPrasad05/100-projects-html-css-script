236126d9333f2f7bffba3a6babe2080aecd6f638
vikasprasad05 <vikasprasad888999@gmail.com>
2023-01-31 16:24:52 +0530

    digital clock

--- Digital clock/script.js
+++ Digital clock/script.js
@@ -1,21 +1,24 @@
 const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minutes");
 const SecondEl = document.getElementById("Seconds");
-const ampmEl = document.getElementById("ampm")
+const ampmEl = document.getElementById("ampm");
 
 
 
 function updateClock() {
-    let  h = new Date().getHours()
-    let  m = new Date().getMinutes()
-    let  s = new Date().getSeconds()
-    let ampm = "AM"
+    let  h = new Date().getHours();
+    let  m = new Date().getMinutes();
+    let  s = new Date().getSeconds();
+    let ampm = "AM";
     
     
     if(h > 12){
-        h = h - 12
-        ampm = "PM"
+        h = h - 12;
+        ampm = "PM";
     }
+
+    
+    ampmEl.innerText = "PM"
 
     h = h<10 ? "0" + h : h;
     m = m<10 ? "0" + m : m;
@@ -25,9 +28,9 @@ function updateClock() {
     hourEl.innerText = h;
     minuteEl.innerText = m;
     SecondEl.innerText = s;
-    ampmEl,(innerText = ampm);
+    
     setTimeout(() => {
-        updateClock()
+        updateClock();
     },1000)
 }
-updateClock()
+updateClock();
